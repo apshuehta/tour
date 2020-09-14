ceb97f5a4934537626c4385833670a67a7cb5492
Eugeniy Yalatov <yalatov@gmail.com>
2020-09-15 00:35:38 +0700

    version 1.100001

--- client/src/components/create/Form/FormChek.js
+++ client/src/components/create/Form/FormChek.js
@@ -5,15 +5,15 @@ export const FormChek = () =>  {
   const create = useCreate()
   return (
     <>
-          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="run"/><span>Маршруты для пеших прогулок</span></label>
-          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="sauna"/><span>Сауна</span></label>
-          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="fishing"/><span>Рыбная ловля</span></label>
-          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="riding"/><span>Верховная езда</span></label>
-          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="heat"/><span>Парная</span></label>
-          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek}  name="accessibly"/><span>Доступен</span></label>
-          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="allDay"/><span>Круглосуточная стойка регистрации</span></label>
-          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="freeCancel"/><span>Бесплатная отмена бронирования</span></label>
-          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="noCreditCard"/><span>Бронирование без кредитной карты</span></label>
+      <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="run"/><span>Маршруты для пеших прогулок</span></label>
+      <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="sauna"/><span>Сауна</span></label>
+      <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="fishing"/><span>Рыбная ловля</span></label>
+      <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="riding"/><span>Верховная езда</span></label>
+      <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="heat"/><span>Парная</span></label>
+      <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek}  name="accessibly"/><span>Доступен</span></label>
+      <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="allDay"/><span>Круглосуточная стойка регистрации</span></label>
+      <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="freeCancel"/><span>Бесплатная отмена бронирования</span></label>
+      <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="noCreditCard"/><span>Бронирование без кредитной карты</span></label>
     </>
   )
 }
