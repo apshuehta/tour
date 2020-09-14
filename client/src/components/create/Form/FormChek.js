852ec2d520745892491301317b22b7cb61e57faa
Eugeniy Yalatov <yalatov@gmail.com>
2020-09-15 00:29:09 +0700

    version 1.1

--- client/src/components/create/Form/FormChek.js
+++ client/src/components/create/Form/FormChek.js
@@ -10,7 +10,7 @@ export const FormChek = () =>  {
           <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="fishing"/><span>Рыбная ловля</span></label>
           <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="riding"/><span>Верховная езда</span></label>
           <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="heat"/><span>Парная</span></label>
-          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="accessibly"/><span>Доступен</span></label>
+          <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek}  name="accessibly"/><span>Доступен</span></label>
           <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="allDay"/><span>Круглосуточная стойка регистрации</span></label>
           <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="freeCancel"/><span>Бесплатная отмена бронирования</span></label>
           <label><input type="checkbox" className="filled-in" onChange={create.onChangeChek} name="noCreditCard"/><span>Бронирование без кредитной карты</span></label>
